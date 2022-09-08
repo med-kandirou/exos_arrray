a90b90f4dbca46b168c8ae15adb1221d388d903e
kandirou0 <simo.kandirou15@gmail.com>
2022-09-08 11:59:11 +0100

    upload

---
+++ exos array/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //https://www.youtube.com/watch?v=sDeiSjffS5Y
+    //correction des exos
+    //49
+    /*int t[10];
+
+    for(int i=0;i<10;i++){
+        t[i]=0;
+    }
+    for(int i=0;i<10;i++){
+        printf("%d \n",t[i]);
+    }*/
+    //50
+
+    char t[]={'a','e','u','o','i','y'};
+    for(int i=0;i<sizeof(t);i++){
+        printf("%c \n",t[i]);
+    }
+
+
+
+    return 0;
+}
